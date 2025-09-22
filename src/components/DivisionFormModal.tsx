12aa641161b0d8a4ec81a1ba1b754a7c4fe08f28
Jeff Martinez <jeffmartinez@isufst.edu.ph>
2025-09-22 20:53:20 +0800

    chore: remove leftover TSX files causing lint errors

--- src/components/DivisionFormModal.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-// Re-export the JSX implementation so TypeScript files can import the component
-import DivisionFormModalImpl from './DivisionFormModal.jsx';
-
-const DivisionFormModal: React.ComponentType<any> = DivisionFormModalImpl as any;
-export default DivisionFormModal;
